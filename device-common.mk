7456e50228bd138eeaf7a1ddee96c48069041aae
surblazer <matvej2002@gmail.com>
2019-09-24 00:56:13 +0300

    [TEST] Add Camera HAL

--- device-common.mk
+++ device-common.mk
@@ -143,6 +143,11 @@ PRODUCT_PACKAGES += \
     Nfc \
     Tag
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     fsck.f2fs \
