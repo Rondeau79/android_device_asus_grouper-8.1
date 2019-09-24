54aa51236fbefddd6242019fdf9fb167fd494d71
surblazer <matvej2002@gmail.com>
2019-09-24 14:01:56 +0300

    Revert "[TEST] Add Camera HAL"
    This reverts commit 7456e50228bd138eeaf7a1ddee96c48069041aae.

--- device-common.mk
+++ device-common.mk
@@ -143,11 +143,6 @@ PRODUCT_PACKAGES += \
     Nfc \
     Tag
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    android.hardware.camera.provider@2.4-impl \
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     fsck.f2fs \
