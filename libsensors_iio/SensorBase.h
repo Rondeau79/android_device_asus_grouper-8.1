ef7b99e3f43821aceeb5b26df0dd364f0ecdfc7c
surblazer <matvej2002@gmail.com>
2019-09-28 02:33:53 +0300

    Revert "libsensors: Update Sensors to Sensors API 1.3"
    This reverts commit f20271ad7084ce89c2ba91fefbd7155981370efb.

--- libsensors_iio/SensorBase.h
+++ libsensors_iio/SensorBase.h
@@ -95,8 +95,6 @@ public:
     virtual int enable(int32_t handle, int enabled);
     virtual int batch(int handle __unused, int flags __unused,
         int64_t period_ns __unused, int64_t timeout __unused);
-    virtual int flush(int handle __unused);
-
 };
 
 /*****************************************************************************/
