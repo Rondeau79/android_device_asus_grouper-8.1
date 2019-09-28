0b7113de787a2a919644c257755619882014df4b
surblazer <matvej2002@gmail.com>
2019-09-28 15:03:06 +0300

    Revert "liblights: update closer to new light HALs"
    This reverts commit 1691cb276b7ebd84c842d6f46a20b3aaa19d7254.

--- liblights/lights.c
+++ liblights/lights.c
@@ -15,6 +15,7 @@
  */
 
 #define LOG_TAG "lights"
+#define LOGE ALOGE
 
 #include <cutils/log.h>
 
@@ -23,44 +24,29 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <malloc.h>
 
 #include <sys/ioctl.h>
 #include <sys/types.h>
 
 #include <hardware/lights.h>
+#include <hardware/hardware.h>
 
-static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
-
-#define BRIGHTNESS_FILE "/sys/class/backlight/pwm-backlight/brightness"
-
-/**
- * device methods
- */
-
-void init_globals(void)
-{
-    // init the mutex
-    pthread_mutex_init(&g_lock, NULL);
-}
 
 static int write_int(char const *path, int value)
 {
 	int fd;
-	static int already_warned = 0;
-
-	fd = open(path, O_WRONLY);
+	static int already_warned = -1;
+	fd = open(path, O_RDWR);
 	if (fd >= 0) {
 		char buffer[20];
-		size_t bytes = snprintf(buffer, sizeof(buffer), "%d\n", value);
-		if(bytes >= sizeof(buffer)) return -EINVAL;
+		int bytes = sprintf(buffer, "%d\n", value);
 		int amt = write(fd, buffer, bytes);
 		close(fd);
 		return amt == -1 ? -errno : 0;
 	} else {
-		if (already_warned == 0) {
-			ALOGE("write_int failed to open %s\n", path);
+		if (already_warned == -1) {
+			LOGE("write_int failed to open %s\n", path);
 			already_warned = 1;
 		}
 		return -errno;
@@ -71,8 +57,8 @@ static int rgb_to_brightness(struct light_state_t const *state)
 {
 	int color = state->color & 0x00ffffff;
 	return ((77 * ((color >> 16) & 0x00ff))
-		+ (150 * ((color >> 8) & 0x00ff))
-		+ (29 * (color & 0x00ff))) >> 8;
+		+ (150 * ((color >> 8) & 0x00ff)) +
+		(29 * (color & 0x00ff))) >> 8;
 }
 
 static int set_light_backlight(struct light_device_t *dev,
@@ -80,12 +66,9 @@ static int set_light_backlight(struct light_device_t *dev,
 {
 	int err = 0;
 	int brightness = rgb_to_brightness(state);
-
-	if(!dev)
-		return -1;
-
 	pthread_mutex_lock(&g_lock);
-	err = write_int(BRIGHTNESS_FILE, brightness);
+	err = write_int("/sys/class/backlight/pwm-backlight/brightness",
+			brightness);
 	pthread_mutex_unlock(&g_lock);
 
 	return err;
@@ -99,14 +82,12 @@ static int close_lights(struct light_device_t *dev)
 	return 0;
 }
 
-/**
- * module methods
- */
-
 /** Open a new instance of a lights device using name */
 static int open_lights(const struct hw_module_t *module, char const *name,
 		       struct hw_device_t **device)
 {
+	pthread_t lighting_poll_thread;
+
 	int (*set_light) (struct light_device_t *dev,
 			  struct light_state_t const *state);
 
@@ -115,13 +96,9 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 	else
 		return -EINVAL;
 
-	pthread_once(&g_init, init_globals);
+	pthread_mutex_init(&g_lock, NULL);
 
 	struct light_device_t *dev = malloc(sizeof(struct light_device_t));
-
-	if(!dev)
-		return -ENOMEM;
-
 	memset(dev, 0, sizeof(*dev));
 
 	dev->common.tag = HARDWARE_DEVICE_TAG;
@@ -131,6 +108,7 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 	dev->set_light = set_light;
 
 	*device = (struct hw_device_t *)dev;
+
 	return 0;
 }
 
