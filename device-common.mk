f7c5b4cac674eed59f555ef83daeaf197629e1bb
surblazer <matvej2002@gmail.com>
2019-09-29 20:19:47 +0300

    Revert "Remove sensors.grouper"
    This reverts commit 96080dba5c03d89d42132a8d9996f50736080351.

--- device-common.mk
+++ device-common.mk
@@ -104,6 +104,7 @@ PRODUCT_COPY_FILES += \
 # Sensors HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    sensors.grouper \
     sensors.invensense \
     libinvensense_hal \
     libmllite \
