2cea6afb6018d3775fe0f27674b5939c0c8c4432
surblazer <matvej2002@gmail.com>
2019-09-30 22:36:17 +0300

    Binderize sensors HAL

--- device-common.mk
+++ device-common.mk
@@ -105,6 +105,7 @@ PRODUCT_COPY_FILES += \
 # Sensors HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     sensors.grouper \
     libinvensense_hal \
     libmllite \
